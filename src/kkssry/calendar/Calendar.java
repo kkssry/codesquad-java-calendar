1575dd70d4109179bb461e4e14635beefe87f68d
kkssry <kkssry@gmail.com>
2018-07-24 17:08:11 +0900

    Update using class method

--- src/kkssry/calendar/Calendar.java
+++ src/kkssry/calendar/Calendar.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class Calendar {
 	
-	public static void main(String[] args) {
-//		System.out.println("Hello, Calendar");
+	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+	
+	public void printSampleCalendar() {
 		System.out.println("일  월  화  수  목  금  토  일");
 		System.out.println("--------------");
 		int[] calendar = new int[28];
@@ -13,18 +18,22 @@ public class Calendar {
 		for(int i=0;i<4;++i) {
 			for(int j=0;j<=6;++j) {
 				calendar[j] = num++;
-				// System.out.print();
 				System.out.print(" "+calendar[j] + " ");
 			}
 			System.out.println();
 		}
+	}
+	
+	public static void main(String[] args) {
+		
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scan = new Scanner(System.in);
-		 System.out.println("달을 입력하세요");
+		Calendar cal = new Calendar();
+		System.out.println("달을 입력하세요");
 		int month = scan.nextInt();
-		int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
-		System.out.println("입력하는 달의 일수는 " + maxDays[month-1] + "일 입니다.");
+		System.out.println("입력하는 달의 일수는 " +cal.getMaxDaysOfMonth(month) + "일 입니다.");
+		cal.printSampleCalendar();
 		scan.close();
 	}
 }
