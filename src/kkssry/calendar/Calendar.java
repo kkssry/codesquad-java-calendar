232f18af69ad42dba228cf0eaab77eadbba2b3d7
kkssry <kkssry@gmail.com>
2018-07-24 16:26:08 +0900

    Update calendar main()

--- src/kkssry/calendar/Calendar.java
+++ src/kkssry/calendar/Calendar.java
@@ -1,11 +1,20 @@
 package kkssry.calendar;
 
 public class Calendar {
-
+	
 	public static void main(String[] args) {
-
-		System.out.println("Hello, Calendar");
-
+//		System.out.println("Hello, Calendar");
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("--------------");
+		int[] calendar = new int[28];
+		int num = 1;
+		for(int i=0;i<4;++i) {
+			for(int j=0;j<=6;++j) {
+				calendar[j] = num++;
+//				System.out.print();
+				System.out.print(calendar[j] + " ");
+			}
+			System.out.println();
+		}
 	}
-
 }
