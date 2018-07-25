8bf5569206d29e5ab5141226ebfbdcdbb52d92e5
kkssry <kkssry@gmail.com>
2018-07-25 09:44:31 +0900

    Update month condition

--- src/kkssry/calendar/Calendar.java
+++ src/kkssry/calendar/Calendar.java
@@ -30,22 +30,32 @@ public class Calendar {
 	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		
+		String PROMPT ="cal> ";
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
 		System.out.println("반복횟수를 입력하세요.");
 		int repeat = scan.nextInt();
-		for(int i=0;i<repeat;++i) {
+		
+		int month=0;
+		while(true) {
 			System.out.println("달을 입력하세요");
-			int month = scan.nextInt();
+			System.out.print(PROMPT);
+			month = scan.nextInt();
+			if(month<0||month>12) {
+				System.out.println("Have a nice day!");
+				break;
+			}
+			
 			System.out.printf("%d월은 %d일까지 있습니다.",month,cal.getMaxDaysOfMonth(month));
 			System.out.println();
 		}
 		System.out.println("bye~~");
 		
 		System.out.println("달을 입력하세요");
-		int month = scan.nextInt();
-		System.out.println("입력하는 달의 일수는 " + cal.getMaxDaysOfMonth(month) + "일 입니다.");
+		int month1 = scan.nextInt();
+		System.out.println("입력하는 달의 일수는 " + cal.getMaxDaysOfMonth(month1) + "일 입니다.");
 		cal.printSampleCalendar();
 		System.out.println("------------");
 		
